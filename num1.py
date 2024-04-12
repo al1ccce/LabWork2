6f6938a9908a1385b29c3393167912636e6e80fa
al1ccce <96458271+al1ccce@users.noreply.github.com>
2024-04-12 17:44:47 +0700

    исправлено 1 задание

--- num1.py
+++ num1.py
@@ -1,37 +1,31 @@
-def moves(hodi, n, m):
+def moves(hodi, n, m, nlocal, mlocal):
     for hod in hodi:
-        if hod == 'L':
-            m -= 1
-        if hod == 'R':
-            m += 1
-        if hod == 'D':
-            n += 1
-        if hod == 'U':
-            n -= 1
-        else:
-            print(hod, " is undefined")
-            return -123, 0
+        match hod:
+            case 'L':
+                m -= 1
+            case 'R':
+                m += 1
+            case 'D':
+                n += 1
+            case 'U':
+                n -= 1
+            case _:
+                print(hod, " is undefined")
+                return -123, 0
+        if n <= 0 or n > nlocal or m <= 0 or m > nlocal:
+            return 0, 0
     return n, m
 
-
-print("n = ")
-n = int(input())
-print("m = ")
-m = int(input())
-print("moves = ")
-hodi = str(input())
-isDone = False
+n = int(input("n = "))
+m = int(input("m = "))
+hodi = str(input("moves = "))
 for i in range(1, n + 1):
     for j in range(1, m + 1):
-        nnew, mnew = moves(hodi, i, j)
+        nnew, mnew = moves(hodi, i, j, n, m)
         if ((nnew > 0 and nnew <= n) and (mnew > 0 and mnew <= m)):
             print(i, j)
-            isDone = True
-            break
+            exit()
         if nnew == -123:
             print('No')
             exit()
-    if isDone:
-        break
-if not isDone:
-    print('No')
+print('No')
