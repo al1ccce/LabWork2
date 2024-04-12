94d72048f32ce0e1abb123455775c50a2abcd716
al1ccce <96458271+al1ccce@users.noreply.github.com>
2024-04-12 18:49:43 +0700

    num1 was changed

--- num1.cs
+++ num1.cs
@@ -1,65 +1,74 @@
 using System;
+
 namespace ChessMoves
 {
     class Program
     {
-        static Tuple<int, int> Moves(string hodi, int n, int m)
+        static Tuple<int, int> Moves(string hodi, int n, int m, int nlocal, int mlocal)
         {
+            int nPos = n;
+            int mPos = m;
+
             foreach (char hod in hodi)
             {
                 switch (hod)
                 {
                     case 'L':
-                        m--;
+                        mPos--;
                         break;
                     case 'R':
-                        m++;
+                        mPos++;
                         break;
                     case 'D':
-                        n++;
+                        nPos++;
                         break;
                     case 'U':
-                        n--;
+                        nPos--;
                         break;
                     default:
                         Console.WriteLine(hod + " is undefined");
                         return Tuple.Create(-123, 0);
                 }
+
+                if ((mPos <= 0 || mPos > mlocal) || (nPos <= 0 || nPos > nlocal))
+                {
+                    return Tuple.Create(0, 0);
+                }
             }
-            return Tuple.Create(n, m);
+
+            return Tuple.Create(nPos, mPos);
         }
 
         static void Main(string[] args)
         {
             int n, m = 0;
             Console.Write("n = ");
-            n = int.Parse(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
             Console.Write("m = ");
-            m = int.Parse(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("moves = ");
             string hodi = Console.ReadLine();
 
             for (int i = 1; i <= n; i++)
             {
                 for (int j = 1; j <= m; j++)
                 {
-                    Tuple<int, int> p = Moves(hodi, i, j);
-                    if (p.Item1 > 0 && p.Item1 <= n && p.Item2 > 0 && p.Item2 <= m)
+                    Tuple<int, int> p = Moves(hodi, i, j, n, m);
+
+                    if ((p.Item1 > 0 && p.Item1 <= n) && (p.Item2 > 0 && p.Item2 <= m))
                     {
                         Console.WriteLine(i + " " + j);
-                        goto exist;
+                        Environment.Exit(1);
                     }
+
                     if (p.Item1 == -123)
                     {
-                        goto doesntexist;
+                        Environment.Exit(-123);
                     }
                 }
             }
 
-            doesntexist:
             Console.WriteLine("No");
-
-            exist:
-            return;
         }
     }
 }
